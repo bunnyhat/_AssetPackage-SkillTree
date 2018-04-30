446af68f8f76b348f10eb3f5140d57730c976722
Zee <zee_shan265@hotmail.com>
2018-04-29 22:24:45 -0400

    functioning and now making an inspector editor

---
+++ Assets/Editor/CustomSkillTreeEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SkillPointGenerator))]
+public class CustomSkillTreeEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        SkillPointGenerator skillPointGenerator = (SkillPointGenerator)target;
+
+        GUILayout.BeginHorizontal();
+        //Generates a skill point with the given values
+        if(GUILayout.Button("Generate Skill")) {
+            skillPointGenerator.GenerateSkill();
+        }
+        //Resets values back to null
+        if(GUILayout.Button("Reset Values")) {
+            skillPointGenerator.name = "";
+            skillPointGenerator.description = "";
+            skillPointGenerator.skillImage = null;
+            skillPointGenerator.minValue = 0;
+            skillPointGenerator.maxValue = 0;
+            skillPointGenerator.isLocked = false;
+        }
+        GUILayout.EndHorizontal();
+
+
+
+    }
+	
+}
---
+++ Assets/Scripts/SkillPointGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillPointGenerator : MonoBehaviour {
+
+	public new string name;
+	public string description;
+	public Sprite skillImage;
+	public int minValue, maxValue;
+	public bool isLocked;
+	
+	public void GenerateSkill() {
+		
+	}
+	
+}
